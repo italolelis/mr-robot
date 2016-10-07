7acc36dd0947d1c81613e22c7e4695e0e0df8b51
Italo Lelis de Vietro <italolelis@gmail.com>
2016-10-07 16:27:18 +0200

    Handling env variables

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -10,6 +10,18 @@ npm install babel-cli
 npm install rimraf
 npm install
 
+echo "Creating environment variables"
+sed -i -e "s/slack-webhook/${SLACK_WEBHOOK}/g" .env.dist
+sed -i -e "s/slack-channel/${SLACK_CHANNEL}/g" .env.dist
+sed -i -e "s/weather-apikey/${WEATHER_API_KEY}/g" .env.dist
+sed -i -e "s/api-v2-url/${API_V2_URL}/g" .env.dist
+sed -i -e "s/weather-url/${WEATHER_URL}/g" .env.dist
+sed -i -e "s/client-id/${CLIENT_ID}/g" .env.dist
+sed -i -e "s/client-secret/${CLIENT_SECRET}/g" .env.dist
+sed -i -e "s/test-country/${TEST_COUNTRY}/g" .env.dist
+
+cp .env.dist .env
+
 echo "Compiling the app"
 npm run compile
 
@@ -19,4 +31,3 @@ echo "Create zip file"
 echo "Coping the zip file to the artifacts folder"
 cd ..
 cp app-master/lib/mr-robot.zip artifacts/
-
